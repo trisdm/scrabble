425ffd1d80fcd640654a6610688bff4512ad7160
Your Name <you@example.com>
2024-01-14 19:48:12 +0000

    user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,14 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('user_type', [UserType::ADMIN->value, UserType::USER->value])->
+            default(UserType::USER->value);
+            $table->enum('user_status', [UserStatus::ACTIVE->value, UserStatus::DELETED->value])->
+            default(UserStatus::ACTIVE->value);
+            $table->integer('average_score')->nullable();
+            $table->integer('highest_score')->nullable();
+            $table->integer('highest_score_game_id')->nullable();
+            $table->dateTime('highest_score_date')->nullable();
             $table->timestamps();
         });
     }
